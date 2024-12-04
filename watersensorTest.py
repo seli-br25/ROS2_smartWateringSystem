bdd418adc8018d769fc5333b5f1a8d2aa75c7130
Selina Breuer <selina.breuer@tuwien.ac.at>
2024-12-04 18:06:49 +0100

    watersensor test for pin17

--- watersensorTest.py
+++ watersensorTest.py
@@ -1,28 +1,17 @@
-import spidev
+import RPi.GPIO as GPIO
 from time import sleep
 
-# SPI initialisieren
-spi = spidev.SpiDev()
-spi.open(0, 0)  # SPI-Bus 0, Gerät 0 (CE0)
-spi.max_speed_hz = 1350000
+MOISTURE_PIN = 17  # GPIO pin for the water sensor
 
-# Funktion zum Lesen eines ADC-Kanals
-def read_adc(channel):
-    assert 0 <= channel <= 7, "Channel muss zwischen 0 und 7 liegen"
-    # SPI-Nachricht senden: Start-Bit, Kanalnummer, Leerdaten
-    adc = spi.xfer2([1, (8 + channel) << 4, 0])
-    # Debug: Zeige die rohen SPI-Daten an
-    print(f"Raw SPI Response: {adc}")
-    # Ergebnis aus zwei Bytes zusammenfügen
-    data = ((adc[1] & 3) << 8) + adc[2]
-    return data
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(MOISTURE_PIN, GPIO.IN)
 
 try:
     while True:
-        value = read_adc(4)  # Kanal 4 (AN-Pin) auslesen
-        print(f"CH1 (Unten1-Pin) ADC-Wert: {value}")
+        if GPIO.input(MOISTURE_PIN) == GPIO.HIGH:
+            print("Moisture detected!")
+        else:
+            print("Soil is dry!")
         sleep(1)
 except KeyboardInterrupt:
-    # SPI-Verbindung schließen, wenn das Programm beendet wird
-    spi.close()
-    print("SPI-Verbindung geschlossen.")
+    GPIO.cleanup()
